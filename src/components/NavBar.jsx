fa86c702c258f9b90f465bb52b4b894ad67f4ef5
hembee <hembee999@gmail.com>
2023-07-14 20:53:17 +0100

    scroll effect

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,39 +1,42 @@
 import styles from "../styles/NavBar.module.css";
 import Tony from "../images/Tony.png";
-import { Link } from "react-scroll";
+import { Link, animateScroll as scroll } from "react-scroll";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+    scroll.scrollTo(link, {
+      duration: 500,
+      offset: -70,
+      smooth: true,
+    });
+  };
+
   return (
     <header className={styles.header}>
       <img className={styles.logo} src={Tony} alt="logo" />
       <nav className={styles.nav}>
         <Link
-          to="#home"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}
-          activeClass={styles.active}
+          to="home"
+          className={activeLink === "home" ? styles.active : ""}
+          onClick={() => handleLinkClick("home")}
         >
           Home
         </Link>
         <Link
-          to="#skills"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}
-          activeClass={styles.active}
+          to="skills"
+          className={activeLink === "skills" ? styles.active : ""}
+          onClick={() => handleLinkClick("skills")}
         >
           Skills
         </Link>
         <Link
-          to="#projects"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={500}
-          activeClass={styles.active}
+          to="projects"
+          className={activeLink === "projects" ? styles.active : ""}
+          onClick={() => handleLinkClick("projects")}
         >
           Projects
         </Link>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,12 @@
 import styles from "../styles/Projects.module.css";
-import pic1 from "../images/portfolio.png"
-import pic2 from "../images/udemy.png"
-import pic3 from "../images/airbnb.png"
-import pic4 from "../images/athletic.png"
-import pic5 from "../images/cash2go.png"
-import pic6 from "../images/drummersville.png"
+import pic1 from "../images/portfolio.png";
+import pic2 from "../images/udemy.png";
+import pic3 from "../images/airbnb.png";
+import pic4 from "../images/athletic.png";
+import pic5 from "../images/cash2go.png";
+import pic6 from "../images/drummersville.png";
+import { Element } from "react-scroll";
+
 const Projects = () => {
   const projects = [
     {
@@ -45,32 +47,34 @@ const Projects = () => {
     },
   ];
   return (
-    <section className={styles.projects} id="projects">
-      <h1>Projects</h1>
-      <p className={styles.projectsp}>
-        Constantly learning and expanding my skill set to stay on top of the
-        latest trends and technologies in full stack development.
-      </p>
-      <div className={styles.project}>
-        {projects.map((project, index) => {
-          return (
-            <a
-              key={index}
-              className={`${styles.projectCard} ${styles.link}`}
-              href={project.url}
-              rel="noreferrer noopener"
-              target= "_blank"
-            >
-              <div>
-                <h2>{project.title}</h2>
-                <p>{project.description}</p>
-              </div>
-              <img src={project.imgUrl} alt="image1" />
-            </a>
-          );
-        })}
-      </div>
-    </section>
+    <Element name="projects">
+      <section className={styles.projects} id="projects">
+        <h1>Projects</h1>
+        <p className={styles.projectsp}>
+          Constantly learning and expanding my skill set to stay on top of the
+          latest trends and technologies in full stack development.
+        </p>
+        <div className={styles.project}>
+          {projects.map((project, index) => {
+            return (
+              <a
+                key={index}
+                className={`${styles.projectCard} ${styles.link}`}
+                href={project.url}
+                rel="noreferrer noopener"
+                target="_blank"
+              >
+                <div>
+                  <h2>{project.title}</h2>
+                  <p>{project.description}</p>
+                </div>
+                <img src={project.imgUrl} alt="image1" />
+              </a>
+            );
+          })}
+        </div>
+      </section>
+    </Element>
   );
 };
 
